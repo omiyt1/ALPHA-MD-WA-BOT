b583fb7bffed51ed7e73d5af157c75d891ce5b84
omiyt1 <115331367+omiyt1@users.noreply.github.com>
2022-10-08 08:19:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,18 +12,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
-global.ownername = 'Achi Fernando'
-global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
+global.owner = ['94789924981']
+global.premium = ['94789924981']
+global.ownernomer = '94789924981'
+global.ownername = '𝙾𝙼𝙸𝚈𝙰𝙰 😌💔'
+global.botname = '𝙾𝙼𝙸𝚈𝙰𝙰 𝙱𝙾𝚃 😐💔'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
 global.ig = 'https://github.com/ALPHA-OFFICIAL-TEAM'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/ALPHA-OFFICIAL-TEAM'
 global.myweb = 'https://www.youtube.com/c/AchiBrolk'
-global.packname = 'ALPHA WA BOT MD'
-global.author = 'D.U.A.N.FERNANDO '
+global.packname = '𝙾𝙼𝙸𝚈𝙰𝙰 𝙱𝙾𝚃 😌💔'
+global.author = '𝙾𝙼𝙸𝚈𝙰𝙰 😐💔'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
